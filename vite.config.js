d182ad8416595e25b4134568c5b032bbeed32453
heneli627627 <102131609+hen627@users.noreply.github.com>
2023-10-19 01:34:06 +0200

    Updated config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/resumewebsite",
+  // base: "/resumewebsite",
   plugins: [react()],
 })
+
+// base code is for github pages perhaps.
